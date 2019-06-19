5e1c5af7b08ccb6491420818b3cf16d5c5155dda
sergeblr <sergeblr@mail.ru>
2019-06-19 18:59:00 +0300

    test1

---
+++ delivery_cost/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //
+        System.out.println("Hi!!!");
+        Integer i = 12;
+
+        System.out.println(i);
+
+    }
+}
