a408cd3f31b1b2f7a924776ee7674cd58ea337af
Siarhei Ivaniukovich <49528951+sergeblr@users.noreply.github.com>
2019-06-26 18:55:15 +0300

    Update Main.java

--- delivery_cost/src/Main.java
+++ delivery_cost/src/Main.java
@@ -11,32 +11,6 @@ public class Main {
         BigDecimal pricePerKg = new BigDecimal("30");
         BigDecimal pricePerKm = new BigDecimal("50");
 
-        /* exported to inputMethod !!!
-        Scanner scanner = new Scanner(System.in);
-        // WEIGHT INPUT:
-        System.out.println("Enter the weight in kg, or press \"q\" to exit");
-        String inputString = scanner.nextLine();
-        if (!inputString.toLowerCase().equals("q")) {
-            weight = new BigDecimal(inputString);
-        }
-        else {
-            System.out.println("\nBye!");
-            return;
-        }
-
-
-        // DISTANCE INPUT:
-        System.out.println("Enter the distance in km, or press \"q\" to exit");
-        inputString = scanner.nextLine();
-        if (!inputString.toLowerCase().equals("q")) {
-            distance = new BigDecimal(inputString);
-        }
-        else {
-            System.out.println("\nBye!");
-            return;
-        }
-        */
-
         // Added with inputMethod:
         weight=inputMethod(1);
         if(weight.equals(BigDecimal.ZERO))
