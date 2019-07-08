fd26be70f6df37236ba44565950220feeb1d93f3
sergeblr <sergeblr@mail.ru>
2019-07-08 19:39:16 +0300

    added cafe-menu Maven prj

---
+++ cafe-menu/src/test/java/com/epam/jsummer19/AppTest.java
@@ -0,0 +1,20 @@
+package com.epam.jsummer19;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ delivery_cost/src/input/ExitInputValue.java
@@ -0,0 +1,8 @@
+package input;
+
+public class ExitInputValue implements InputValue {
+    @Override
+    public Types getType() {
+        return Types.EXIT;
+    }
+}
