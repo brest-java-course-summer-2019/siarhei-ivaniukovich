2add506dff2c9ab8e23d000d848eb61413dd6b8d
Siarhei Ivaniukovich <sergeblr@mail.ru>
2019-08-13 15:27:54 +0300

    model: OrdersList removed, ItemInOrder added (+ TESTS) dao: OrdersListDao removed, ItemInOrderDao+IMPL added todo: #ItemInOrderDaoJdbcImpl (WRITE TO DB!!!), TESTS

--- cafemenu/test-db/src/main/resources/create-tables.sql
+++ cafemenu/test-db/src/main/resources/create-tables.sql
@@ -8,7 +8,6 @@ CREATE TABLE item_table (
 
 DROP TABLE IF EXISTS order_table;
 CREATE TABLE order_table (
-<<<<<<< HEAD
   order_id INT NOT NULL AUTO_INCREMENT,
   order_employee_id INT NOT NULL,
   order_status INT NOT NULL,
@@ -24,20 +23,4 @@ CREATE TABLE item_in_order (
   iio_item_count INT NOT NULL,
   PRIMARY KEY (iio_item_id, iio_order_id),
   FOREIGN KEY (iio_order_id) REFERENCES order_table(order_id)
-=======
-  order_id INT NOT NULL,
-  order_item_id INT NOT NULL,
-  order_item_count INT NOT NULL,
-  FOREIGN KEY (order_item_id) REFERENCES item_table(item_id),
-  PRIMARY KEY (order_id, order_item_id)
-);
-
-DROP TABLE IF EXISTS orderslist_table;
-CREATE TABLE orderslist_table (
-  ol_order_id INT NOT NULL AUTO_INCREMENT,
-  ol_employee_id INT NOT NULL,
-  ol_order_status INT NOT NULL,
-  FOREIGN KEY (ol_order_id) REFERENCES order_table(order_id),
-  PRIMARY KEY (ol_order_id)
->>>>>>> 12411dfee4c748110da942719896b9673b8ed7d1
 );
