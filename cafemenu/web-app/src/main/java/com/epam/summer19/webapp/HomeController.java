4c44a6ac2a0c46ce03d9818393614fccc2166005
Siarhei Ivaniukovich <sergeblr@mail.ru>
2019-08-22 14:54:42 +0300

    web-app work1

--- cafemenu/web-app/src/main/java/com/epam/summer19/webapp/HomeController.java
+++ cafemenu/web-app/src/main/java/com/epam/summer19/webapp/HomeController.java
@@ -1,5 +1,7 @@
 package com.epam.summer19.webapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,12 +12,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     /**
+     * Logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
+    /**
      * Redirect to default page -> main.html
      *
      * @return redirect path
      */
     @GetMapping(value = "/")
-    public String defaultPageRedirect() {
+    public final String defaultPageRedirect() {
+        LOGGER.debug("defaultPageRedirect()");
         return "redirect:main";
     }
+
+}
 
