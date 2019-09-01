5839d860ccd4bb23c1e3ad0e7228240bb3737516
sergeblr <sergeblr@mail.ru>
2019-09-01 10:05:02 +0300

    restapp: ItemRestController test added

--- cafemenu/rest-app/src/test/java/com/epam/summer19/restapp/ItemRestControllerTest.java
+++ cafemenu/rest-app/src/test/java/com/epam/summer19/restapp/ItemRestControllerTest.java
@@ -1,4 +1,4 @@
-package com.epam.summer19.restapp.;
+package com.epam.summer19.restapp;
 
 import com.epam.summer19.model.Item;
 import com.epam.summer19.service.ItemService;
@@ -21,6 +21,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,7 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath:rest-spring-test.xml"})
-public class DepartmentRestControllerTest {
+public class ItemRestControllerTest {
 
     @Autowired
     private ItemRestController itemController;
@@ -58,46 +59,20 @@ public class DepartmentRestControllerTest {
         Mockito.reset(itemService);
     }
 
-    @Test
-    public void departments() throws Exception {
-
-        Mockito.when(service.findAll()).thenReturn(Arrays.asList(createDepartmentFixture(0), createDepartmentFixture(1)));
-
-        mockMvc.perform(
-                MockMvcRequestBuilders.get("/departments")
-                        .accept(MediaType.APPLICATION_JSON_UTF8)
-        ).andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].departmentName", Matchers.is("def0")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].departmentId", Matchers.is(0)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].departmentName", Matchers.is("def1")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].departmentId", Matchers.is(1)))
-        ;
-
-        Mockito.verify(service).findAll();
-    }
-
-    private Department createDepartmentFixture(int index) {
-        Department department = new Department();
-        department.setDepartmentName("def" + index);
-        department.setDepartmentId(index);
-        return department;
-    }
 
     @Test
-    public void shouldPersistDepartment() throws Exception {
+    public void testItemAdd() throws Exception {
 
-        Department expectedDepartment = createDepartmentFixture(3);
+        Item expectedItem = createItem(1,3);
+        Item inputItem = new Item();
+        inputItem.setItemName(expectedItem.getItemName());
 
-        Department inputDepartment = new Department()
-                .setDepartmentName(expectedDepartment.getDepartmentName());
+        String json = new ObjectMapper().writeValueAsString(inputItem);
 
-        String json = new ObjectMapper().writeValueAsString(inputDepartment);
-
-        Mockito.when(service.add(any(Department.class))).thenReturn(expectedDepartment);
+        Mockito.doNothing().when(itemService).add(any(Item.class));
 
         MockHttpServletResponse response = mockMvc.perform(
-                post("/department")
+                post("/item")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json)
@@ -106,23 +81,70 @@ public class DepartmentRestControllerTest {
                 .andReturn().getResponse();
 
         String content = response.getContentAsString();
-        Department result = objectMapper.readValue(content, Department.class);
-        assertEquals(expectedDepartment.getDepartmentName(), result.getDepartmentName());
-        assertEquals(expectedDepartment.getDepartmentId(), result.getDepartmentId());
-
+        Item result = objectMapper.readValue(content, Item.class);
+        assertEquals(expectedItem.getItemName(), result.getItemName());
+        assertEquals(expectedItem.getItemPrice(), result.getItemPrice());
     }
 
     @Test
-    public void shouldUpdateDepartment() throws Exception {
+    public void testUpdateItem() throws Exception {
+        Item item = createItem(2,4);
+        String json = new ObjectMapper().writeValueAsString(item);
 
-        Department department = createDepartmentFixture(1);
-        String json = new ObjectMapper().writeValueAsString(department);
-
-        mockMvc.perform(put("/department")
+        mockMvc.perform(put("/item")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(json)
         ).andExpect(status().isAccepted());
+
+        Mockito.verify(itemService, Mockito.times(1)).update(any());
+    }
+
+    @Test
+    public void testDeleteItem() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/items/4"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        Mockito.verify(itemService, Mockito.times(1)).delete(any());
+    }
+
+
+    @Test
+    public void testItemFindAll() throws Exception {
+        Mockito.when(itemService.findAll()).thenReturn(Arrays.asList(createItem(1,2), createItem(2,3)));
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/items")
+                        .accept(MediaType.APPLICATION_JSON_UTF8)
+        ).andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].itemName", Matchers.is("Item1")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].itemPrice", Matchers.is(2.2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].itemName", Matchers.is("Item2")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].itemPrice", Matchers.is(2.2)))
+        ;
+        Mockito.verify(itemService).findAll();
+    }
+
+    @Test
+    public void testItemFindById() throws Exception {
+        Mockito.when(itemService.findItemById(1)).thenReturn(createItem(1,5));
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/items/1")
+                        .accept(MediaType.APPLICATION_JSON_UTF8)
+        ).andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].itemName", Matchers.is("Item5")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].itemPrice", Matchers.is(2.2)))
+        ;
+        Mockito.verify(itemService, Mockito.times(1)).findItemById(1);
+    }
+
+    private Item createItem(int itemId, int namePostfix) {
+        Item item = new Item();
+        item.setItemId(itemId);
+        item.setItemName("Item" + namePostfix);
+        item.setItemPrice(new BigDecimal(2.2));
+        return item;
     }
 
 }
