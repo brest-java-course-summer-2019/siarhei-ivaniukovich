fd0fe739069d3cf69d205a6332f2ab4592c50001
sergeblr <sergeblr@mail.ru>
2019-09-21 00:25:37 +0300

    fetching + history from devrep repo

--- cafemenu/web-app/src/test/java/com/epam/summer19/web_app/consumers/OrderRestConsumerTest.java
+++ cafemenu/web-app/src/test/java/com/epam/summer19/web_app/consumers/OrderRestConsumerTest.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
